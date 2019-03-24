41a4bafd1d80a0fb50d4e7b1927ca75ad0420e33
Zaec <Kulevw@gmail.com>
2019-03-24 12:09:51 +0500

    nuxt ts linter sass

---
+++ front-concert/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <test-component />
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+import TestComponent from '../components/TestComponent.vue';
+
+@Component({
+  components: {
+    TestComponent,
+  },
+})
+export default class Home extends Vue {
+
+}
+</script>
---
+++ front-concert/plugins/api/helpers.ts
@@ -0,0 +1,91 @@
+// import { AnyARecord } from 'dns';
+
+export interface IQuery<TForm, TResult> {
+  readonly method: String,
+  readonly url: String,
+  execute(form: TForm): Promise<TResult>
+}
+
+// interface IQueryResult<TResult> {
+//   readonly status: Boolean
+//   readonly data: any
+// }
+
+// class Query<TForm, TResult> implements IQuery<TForm, TResult> {
+//   method: String
+//   url: String
+
+//   execute(form: TForm): Promise<TResult> {
+//     throw new Error('Method not implemented.');
+//   }
+// }
+
+export interface IPaginateResult<TItem> {
+  meta: PaginateMeta
+  items: Array<TItem>
+}
+
+export interface IPaginateMeta {
+  readonly per_page: Number
+  readonly current_page: Number
+  readonly last_page: Number
+  readonly total: Number
+}
+
+export class PaginateMeta {
+  public readonly per_page: Number
+  public readonly current_page: Number
+  public readonly last_page: Number
+  public readonly total: Number
+
+  constructor(meta: IPaginateMeta) {
+    this.per_page = meta.per_page
+    this.current_page = meta.current_page
+    this.last_page = meta.last_page
+    this.total = meta.total
+  }
+
+  public isEmpty() {
+    return this.total <= 0
+  }
+
+  public static readonly empty: PaginateMeta = new PaginateMeta({
+    per_page: 0,
+    current_page: 0,
+    last_page: 0,
+    total: 0
+  })
+}
+
+export class LazyPaginator<TForm, TItem> {
+  private _query: IQuery<TForm, IPaginateResult<TItem>>
+  private _items: ReadonlyArray<TItem>
+  private _meta: PaginateMeta
+
+  public get query(): IQuery<TForm, IPaginateResult<TItem>> {
+    return this._query
+  }
+
+  public get items(): ReadonlyArray<TItem> {
+    return this._items
+  }
+
+  public get meta(): PaginateMeta {
+    return this._meta
+  }
+
+  constructor(query: IQuery<TForm, IPaginateResult<TItem>>) {
+    this._query = query
+    this._items = new Array()
+    this._meta = PaginateMeta.empty
+  }
+
+  public async next(form: TForm): Promise<Boolean> {
+    const result = await this._query.execute(form)
+
+    this._items = this._items.concat(result.items)
+    this._meta = result.meta
+
+    return true
+  }
+}
